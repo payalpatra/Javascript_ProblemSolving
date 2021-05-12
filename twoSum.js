a27203b32664e2fc869b5a2af8e7f86046d531fc
payalpatra <payalpatra105@gmail.com>
2021-05-12 23:56:59 +0530

    Added TwoSum Problem

---
+++ twoSum.js
@@ -0,0 +1,13 @@
+// To return the indices of two numbers in an array such that they add up to a specific target
+
+// Input Data - [2,7,11,15],9
+// Output - 
+// Explanation - 
+
+// SOURCE CODE
+function twoSum(nums, target) {
+
+
+}
+
+console.log(twoSum([2,7,11,15],9));
